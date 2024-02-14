b15442a63979b739b4b2dd825385bfb8011fb5c8
RytoryQA <vikaryzhkina89@gmail.com>
2024-02-14 18:28:59 +0400

    add Homework-JS-3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
+		return true;
+	}
+	return false;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, user, index, users) => {
+		acc += user;
+		if (index === users.length - 1) {
+			let avg = acc / users.length;
+			return avg;
+		}
+		return acc;
+	}, 0)
+	return result;
 }
