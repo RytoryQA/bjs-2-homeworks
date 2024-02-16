538dc1dbf4c32b9801df35b6cb6f44b23dfca41b
RytoryQA <vikaryzhkina89@gmail.com>
2024-02-16 23:21:00 +0400

    add Homework-JS-4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+let student1 = new Student();
+let tudent2 = new Student();
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		console.log("Error: Student has been expelled.");
+		return;
+	}
+	this.marks.push(...marksToAdd);
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || !this.marks.length) {
+		return 0;
+	}
+	return this.marks.reduce((acc, curr) => acc + curr) / this.marks.length;
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
